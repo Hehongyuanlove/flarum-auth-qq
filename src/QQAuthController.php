93861d47bc31e8499332e2577723279c71e7a1bb
Hehongyuanlove <1016823618@qq.com>
2021-11-10 11:00:03 +0800

    修复特殊字符用户名
    只允许 汉字、字母、数字

--- src/QQAuthController.php
+++ src/QQAuthController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Hehongyuanlove\AuthQQ;
 
 use Exception;
@@ -15,7 +16,8 @@ use Zend\Diactoros\Response\RedirectResponse;
 use Illuminate\Support\Str;
 use Illuminate\Support\Arr;
 
-class QQAuthController implements RequestHandlerInterface {
+class QQAuthController implements RequestHandlerInterface
+{
     /**
      * @var ResponseFactory
      */
@@ -36,7 +38,8 @@ class QQAuthController implements RequestHandlerInterface {
      * @param SettingsRepositoryInterface $settings
      * @param UrlGenerator $url
      */
-    public function __construct(QQResponseFactory $response, SettingsRepositoryInterface $settings, UrlGenerator $url){
+    public function __construct(QQResponseFactory $response, SettingsRepositoryInterface $settings, UrlGenerator $url)
+    {
         $this->response = $response;
         $this->settings = $settings;
         $this->url      = $url;
@@ -48,17 +51,18 @@ class QQAuthController implements RequestHandlerInterface {
      * @return ResponseInterface
      * @throws Exception
      */
-    public function handle(Request  $request): ResponseInterface {
-             
-        $redirectUri ="https:".$this->url->to('api')->route('auth.qq');
+    public function handle(Request  $request): ResponseInterface
+    {
+
+        $redirectUri = "https:" . $this->url->to('api')->route('auth.qq');
         $provider   = new QQ([
             'clientId'          => $this->settings->get('hehongyuanlove-auth-qq.client_id'),
             'clientSecret'      => $this->settings->get('hehongyuanlove-auth-qq.client_secret'),
             'redirectUri'       => $redirectUri,
         ]);
-        
-       
-      
+
+
+
         $session        = $request->getAttribute('session');
         $queryParams    = $request->getQueryParams();
         $code           = Arr::get($queryParams, 'code');
@@ -69,50 +73,56 @@ class QQAuthController implements RequestHandlerInterface {
             return new RedirectResponse($authUrl);
         }
         $state          = Arr::get($queryParams, 'state');
-        
-       // var_dump($state,$session->get('oauth2state'));
-        
+
+        // var_dump($state,$session->get('oauth2state'));
+
         if (!$state || $state !== $session->get('oauth2state')) {
             // $session->remove('oauth2state');
             // throw new Exception('Invalid state');
-             $session->put('oauth2state', $state);
+            $session->put('oauth2state', $state);
         }
-        
+
         $token          = $provider->getAccessToken('authorization_code', [
             "code"  => $code,
         ]);
-      
+
         $user           = $provider->fetchOpenid($token);
-     
-        $userinfo = $provider->fetchUesrInfo($token,$user['openid']);
- 
-		$userinforesult = array_merge_recursive($user, $userinfo);
-		
-		$actor = $request->getAttribute('actor');
-		
-		$loginResultRes = $this->response->make(
-            'QQ', $userinforesult["openid"],$actor,
+
+        $userinfo = $provider->fetchUesrInfo($token, $user['openid']);
+
+        $userinforesult = array_merge_recursive($user, $userinfo);
+
+        $actor = $request->getAttribute('actor');
+
+        $loginResultRes = $this->response->make(
+            'QQ',
+            $userinforesult["openid"],
+            $actor,
             function (Registration $registration) use ($userinforesult) {
                 $registration
                     // ->suggestEmail(str::upper(str::random(20)) . "@qq.com")
                     //->suggestUsername($userinforesult["nickname"].str::upper(str::random(4)))
-                    ->provide("username",$userinforesult["nickname"].str::upper(str::random(4)))
-                    ->provide("email","himi3d.".str::upper(str::random(20)) . "@qq.com")
+                    ->provide("username",  $this->UserNameMatch($userinforesult["nickname"]) . str::upper(str::random(4)))
+                    ->provide("email", "himi3d." . str::upper(str::random(20)) . "@qq.com")
                     ->provideAvatar($userinforesult['figureurl_qq_2'])
                     ->setPayload($userinforesult);
             }
         );
-        
-        return $loginResultRes;
-        
 
-        
-        // return $loginResultRes;
-        // 这里省去判断前面是否登录
-        return new HtmlResponse('<script>11</script>');
-        // return new HtmlResponse('<script>window.location.href = "/"; window.app.authenticationComplete({"loggedIn":true});</script>');
+        return $loginResultRes;
+
+
+
+        // // return $loginResultRes;
+        // // 这里省去判断前面是否登录
+        // return new HtmlResponse('<script>11</script>');
+        // // return new HtmlResponse('<script>window.location.href = "/"; window.app.authenticationComplete({"loggedIn":true});</script>');
 
     }
-    
-}
 
+    public function UserNameMatch($str)
+    {
+        preg_match_all('/[\x{4e00}-\x{9fa5}a-zA-Z0-9]/u', $str, $result);
+        return implode('', $result[0]);
+    }
+}
